3a29d39d0702519f1eae04b55933aca43d165b1e
Andreu Vallbona Plazas <avallbona@gmail.com>
2019-04-20 12:30:04 +0200

    Simplified return statement of the send_message method
    Simplified return statement of the send_message method

--- telegram_bot.py
+++ telegram_bot.py
@@ -65,7 +65,7 @@ class TelegramBot:
 
         res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.outgoing_message_text))
 
-        return True if res.status_code == 200 else False
+        return res.status_code == 200
     
 
     @staticmethod
